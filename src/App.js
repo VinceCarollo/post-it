72d3a73c683709d73c1acd4c7fc63e028de2dc64
vincecarollo <vincecarollo@gmail.com>
2020-10-04 11:15:56 -0600

    Set up post it component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 id="post-it-header">Post It</h1>
-    </div>
-  )
+    useEffect(() => {
+        
+    }, [])
+
+    return (
+        <div className="App">
+            <h1 id="post-it-header">Post It</h1>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/PostItForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PostItForm() {
+    return (
+        
+    )
+}
+
+export default PostItForm;
